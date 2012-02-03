11337c71f9cb33e3313b4b098da678fbe8ef5dbd
mike.priest <mike.priest@abstractive.ca>
2012-02-03 00:03:35 +0000

    Playing around with some stuff

--- modules/formsmanagement/config/alfresco/templates/webscripts/ca/ab/fm/formsmanagement/other-services/gmail/user-settings.get.js
+++ modules/formsmanagement/config/alfresco/templates/webscripts/ca/ab/fm/formsmanagement/other-services/gmail/user-settings.get.js
@@ -1,11 +1,34 @@
-var username = args.user;
-var settingNode = userhome.childByNamePath('gmailSettings.json');
-if(settingNode){
-	model.json = settingNode.content;
-}else{
-	var newSetting = userhome.createFile('gmailSettings.json');
-	newSetting.content = '{ "username": "", "password": "", "mailServer": "", "ssl": 0, "port": "", "labels": {} }';
-	newSetting.save();
-	
-	model.json = newSetting.content;
+function getUserSettings(){
+	var json = "{}";
+	var settingNode = userhome.childByNamePath('gmailSettings.json');
+	if(settingNode){
+		json = settingNode.content;
+	}else{
+		var newSetting = userhome.createFile('gmailSettings.json');
+		newSetting.content = '{ "username": "", "password": "", "mailServer": "", "ssl": 0, "port": "", "labels": {} }';
+		newSetting.save(); 
+		json = newSetting.content;  
+	}
+	return json;
 }
+function getRMSettings(siteid){
+	var json = "{ ";
+	var settingNode = companyhome.childByNamePath('Sites/'+siteid+'/rm-file-mapping.json');
+	var rmObj = eval("("+settingNode.content+")");
+	for(i in rmObj){
+		var tmpNode = companyhome.childByNamePath('Sites/rm/documentLibrary'+rmObj[i]);
+		if(tmpNode){
+			json += '"'+i+'": "'+tmpNode.id+'",';
+		}
+	}  
+	json = json.slice(0, -1) + "}";
+	return json;
+}
+ 
+var user = getUserSettings(); 
+var rm = getRMSettings(args.siteid);  
+var appSettings = '{';
+appSettings += '"user" :' + user + ',';
+appSettings += '"rmMap" :' + rm;
+appSettings += '}';
+model.json = appSettings;
