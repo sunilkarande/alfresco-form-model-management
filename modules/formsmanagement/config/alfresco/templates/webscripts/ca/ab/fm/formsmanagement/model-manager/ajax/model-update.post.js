ccf83fc5c40277fe54a46bed5122b286e5654bcb
mike.priest <mike.priest@abstractive.ca>
2012-09-12 17:17:21 +0000

    Fixed. Json was trying to validate against model on aspect add. It cannot validate something that doesn't exist.

--- modules/formsmanagement/config/alfresco/templates/webscripts/ca/ab/fm/formsmanagement/model-manager/ajax/model-update.post.js
+++ modules/formsmanagement/config/alfresco/templates/webscripts/ca/ab/fm/formsmanagement/model-manager/ajax/model-update.post.js
@@ -2,7 +2,7 @@
 <import resource="classpath:alfresco/templates/webscripts/ca/ab/fm/formsmanagement/utils/model/convertJsonToModel.js">
 <import resource="classpath:alfresco/templates/webscripts/ca/ab/fm/formsmanagement/utils/locale/alfresco-locale.js">
 
-function saveJson(jsonNode, jsonObject){
+function saveJsonAndValidate(jsonNode, jsonObject){
 
 	var xmlString = convertJsonToXml(jsonObject);
 	model.status = 1;
@@ -26,6 +26,16 @@ function saveJson(jsonNode, jsonObject){
 	}
 }
 
+function saveJson(jsonNode, jsonObject){
+
+	var newObjString = JSON.stringify(jsonObject);
+	//Remove NULLS
+	newObjString = newObjString.replace(/,null/g, "");
+	jsonNode.content = newObjString;
+	jsonNode.save();
+
+}
+
 var aspectName = args.aspect;
 var prefix = args.prefix;
 var action = args.action;
@@ -36,6 +46,9 @@ var jsonNode = search.findNode("workspace://SpacesStore/" + args.jsonid);
 var jObj = eval("(" + jsonNode.content + ")");
 
 if(action == "add"){
+
+	logger.log("GMAIL:" + jsonNode.content);
+
 	var addTemplate = {"title":"#ASPECT#","visible":true,"namespace":"#PREFIX#","name":"#ASPECT#","description":"Default example description","properties":[]};
 	addTemplate.title = aspectName;
 	addTemplate.name = aspectName;
@@ -59,7 +72,7 @@ if(action == "add"){
 			delete jObj.aspects[i];
 
 			//Save node back
-			saveJson(jsonNode, jObj);
+			saveJsonAndValidate(jsonNode, jObj);
 		}
 	}
 }else if(action == "update"){
@@ -73,7 +86,7 @@ if(action == "add"){
 	jObj.imports = saveObj.imports;
 	jObj.namespaces = saveObj.namespaces;
 
-	saveJson(jsonNode, jObj);
+	saveJsonAndValidate(jsonNode, jObj);
 
 
 }
